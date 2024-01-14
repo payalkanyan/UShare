4881f1ec08d885d6c23cb2f2d942e795350d4736
payalkanyan <122166407+payalkanyan@users.noreply.github.com>
2024-01-14 10:54:22 +0530

    Add files via upload

---
+++ UShare_frontend/src/utils/constants.js
@@ -0,0 +1,7 @@
+//contract abi in ./palpal.json
+import abi from "./palpal.json";
+
+export const contractAddress = "0xd1B787a4Cbd10bFf2dB2091FDfBFB254D52af7F3";
+export const contractABI = abi.abi;
+export const API_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEFlNTNkMWM5QzQxNDQ1NzgwMUI5NUEyMzQxZEQwMTAxYWZhOTI1MUMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTk3MTQyMzM0MTksIm5hbWUiOiJ0ZXN0In0.tZ1cpe4MDtdIA4CJkbX_MhoCTJDaruC4YTX5a4W9GxE";
